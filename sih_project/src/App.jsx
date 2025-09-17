51175e0a19822024bca8e6f1b1f5fe0a1283d991
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2025-09-17 21:09:41 +0530

    1st commit

---
+++ sih_project/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold ">
+        This is the official web page of Navikar
+      </h1>
+      <img src="" alt="" />
+    </div>
+  );
+}
+
+export default App;
